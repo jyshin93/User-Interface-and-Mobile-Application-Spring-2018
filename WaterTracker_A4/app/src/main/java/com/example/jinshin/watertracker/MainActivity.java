ac2d4473e93ff4390e7998451b7a8f0ea516f9aa
jinshin <jyshin93@hotmail.com>
2018-03-10 16:57:47 -0500

    complete amount change in settings

--- WaterTracker_A4/app/src/main/java/com/example/jinshin/watertracker/MainActivity.java
+++ WaterTracker_A4/app/src/main/java/com/example/jinshin/watertracker/MainActivity.java
@@ -33,13 +33,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Animation alpha;
     public static final String PREFS_NAME = "MyPrefsFile";
-    float amount;
+    float amount, goal_amount;
     int totalInt;
 
     Setting setting;
-
-
-
     SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     LinearLayout mug, cup, bottle;
@@ -53,18 +50,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     SharedPreferences.Editor editor;
 
     Date date;
-
     String open_date;
+    String base_measurement;
 
     WaveLoadingView wave;
-
     android.support.v7.widget.Toolbar action_bar;
 
     Intent intent;
+    int cup_amount, mug_amount, bottle_amount;
+    TextView cup_text, mug_text, bottle_text, measure_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        totalInt = 40;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -85,6 +82,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //receive amount based on current date
         open_date = getDate();
         amount = settings.getFloat(open_date, 0);
+        goal_amount = settings.getFloat("Goal", 64);
+        base_measurement = settings.getString("Measure", "oz");
 
         //setup animation
         alpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
@@ -93,9 +92,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toast = new Toast(getApplicationContext());
         toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
 
-
         screen = (TextView) findViewById(R.id.screen);
-        screen.setText("You drank " + amount + "/64" +" oz of water today!");
+        screen.setText("You drank " + amount + "/" + goal_amount +" " + base_measurement + " of water today!");
 
         cup = (LinearLayout) findViewById(R.id.cup_layout);
         cup.setOnClickListener(this);
@@ -109,13 +107,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         minus = (TextView) findViewById(R.id.minus);
         minus.setOnClickListener(this);
 
+        cup_text = (TextView) findViewById(R.id.cup_text);
+        mug_text = (TextView) findViewById(R.id.mug_text);
+        bottle_text = (TextView) findViewById(R.id.bottle_text);
+        measure_view = (TextView) findViewById(R.id.measure_view);
+        initialize_amount(base_measurement);
+
         //call image layout
         cup_image_layout_2 = (ImageView) findViewById(R.id.cup_image_layout_2);
         wave = (WaveLoadingView) findViewById(R.id.cup_image_layout_1);
         fillup();
 
         input_text = (EditText) findViewById(R.id.input_text);
-        setting = new Setting();
 
     }
 
@@ -172,8 +175,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        goal_amount = settings.getFloat("Goal", goal_amount);
+        String prev_measurement = base_measurement;
+        base_measurement = settings.getString("Measure", base_measurement);
+        if (!prev_measurement.equals(base_measurement)) {
+            if (base_measurement.equals("oz")) {
+                //convert amount from milli to oz
+                amount = (float) ((int) (amount / 30));
+                //convert button amounts
+                convert_amounts(base_measurement);
+            } else {
+                //convert amount from oz to milli
+                amount = (float) ((int) (amount * 30));
+                //convert button amounts
+                convert_amounts(base_measurement);
+            }
+        }
+        screen.setText(String.format("You drank %.1f/%.1f %s of water today!", amount, goal_amount, base_measurement));
+        measure_view.setText(base_measurement);
+        fillup();
+    }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        editor = settings.edit();
+        editor.putFloat("Goal", goal_amount);
+        editor.commit();
+    }
 
     @Override
     protected void onStop(){
@@ -194,17 +226,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.cup_layout:
                 v.startAnimation(alpha);
-                input_text.setText("8");
+                input_text.setText(String.valueOf(cup_amount));
 //                screen.setText("You drank " + Html.fromHtml(amount + "<font color='#0000FF'></font> ")  + "/64 oz of water today!");
                 break;
             case R.id.mug_layout:
                 v.startAnimation(alpha);
-                input_text.setText("12");
+                input_text.setText(String.valueOf(mug_amount));
 //                screen.setText("You drank " + amount + "/64 oz of water today!");
                 break;
             case R.id.bottle_layout:
                 v.startAnimation(alpha);
-                input_text.setText("17");
+                input_text.setText(String.valueOf(bottle_amount));
 //                screen.setText("You drank " + amount + "/64 oz of water today!");
                 break;
 
@@ -216,10 +248,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     amount += temp;
                     if (amount > 64) {
                         toast.makeText(MainActivity.this, "You have completed goal today!!!", toast.LENGTH_SHORT).show();
-                        screen.setText("You drank " + amount + "/" + setting.getTotal() +" oz of water today!");
+                        screen.setText(String.format("You drank %.1f/%.1f %s of water today!", amount, goal_amount, base_measurement));
                         fillup();
                     } else {
-                        screen.setText("You drank " + amount + "/" + totalInt + " oz of water today!");
+                        screen.setText(String.format("You drank %.1f/%.1f %s of water today!", amount, goal_amount, base_measurement));
                         toast.makeText(MainActivity.this, "Added " + temp + " now", toast.LENGTH_SHORT).show();
                         input_text.setText("");
                         //change bottle image
@@ -241,7 +273,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         input_text.setText("");
                     }else {
                         amount -= temp;
-                        screen.setText("You drank " + amount + "/" + setting.getTotal() + " oz of water today!");
+                        screen.setText(String.format("You drank %.1f/%.1f %s of water today!", amount, goal_amount, base_measurement));
                         toast.makeText(MainActivity.this, "Deducted " + temp + " now", toast.LENGTH_SHORT).show();
                         input_text.setText("");
                         fillup();
@@ -274,7 +306,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void fillup() {
-        float ratio = amount / 64;
+        float ratio = amount / goal_amount;
         int progress_value = (int) (ratio * 100);
         if (ratio <= 1 ) {
             wave.setProgressValue(progress_value);
@@ -289,8 +321,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
-    public void setTotal(int total) {
-        this.totalInt = total;
+
+    public void convert_amounts(String measure) {
+        if (measure.equals("oz")) {
+            //from milli to oz
+            cup_amount = (int) (cup_amount / 30.0);
+            mug_amount = (int) (mug_amount / 30.0);
+            bottle_amount = (int) (bottle_amount / 30.0);;
+            cup_text.setText(String.format("%d %s", cup_amount, measure));
+            mug_text.setText(String.format("%d %s", mug_amount, measure));
+            bottle_text.setText(String.format("%d %s", bottle_amount, measure));
+        } else {
+            //from oz to milli
+            cup_amount *= 30;
+            mug_amount *= 30;
+            bottle_amount *= 30;
+            cup_text.setText(String.format("%d %s", cup_amount, measure));
+            mug_text.setText(String.format("%d %s", mug_amount, measure));
+            bottle_text.setText(String.format("%d %s", bottle_amount, measure));
+        }
+    }
+
+    public void initialize_amount(String measure) {
+        if (measure.equals("oz")) {
+            cup_amount = 8;
+            mug_amount = 12;
+            bottle_amount = 17;
+            cup_text.setText(String.format("%d %s", cup_amount, measure));
+            mug_text.setText(String.format("%d %s", mug_amount, measure));
+            bottle_text.setText(String.format("%d %s", bottle_amount, measure));
+        } else {
+            cup_amount = 8 * 30;
+            mug_amount = 12 * 30;
+            bottle_amount = 17 * 30;
+            cup_text.setText(String.format("%d %s", cup_amount, measure));
+            mug_text.setText(String.format("%d %s", mug_amount, measure));
+            bottle_text.setText(String.format("%d %s", bottle_amount, measure));
+        }
     }
 
 
--- WaterTracker_A4/app/src/main/java/com/example/jinshin/watertracker/Setting.java
+++ WaterTracker_A4/app/src/main/java/com/example/jinshin/watertracker/Setting.java
@@ -1,8 +1,10 @@
 package com.example.jinshin.watertracker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -10,6 +12,8 @@ import android.widget.Button;
 import android.view.View;
 import android.util.Log;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /**
  * Created by jinshin on 05/03/2018.
@@ -17,15 +21,23 @@ import android.widget.RadioButton;
 
 public class Setting extends AppCompatActivity {
 
-    int totalInt = 63;
-    Button okbutton;
     EditText totalEdit, timeEdit;
     String total;
     MainActivity mainActivity;
 
     android.support.v7.widget.Toolbar action_bar;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    SharedPreferences settings;
+    SharedPreferences.Editor editor;
 
-    
+    RadioButton ounces, milli;
+    RadioGroup rg;
+
+    Toast toast;
+
+    String base_measurement;
+    float goal;
+
     /*
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,48 +48,60 @@ public class Setting extends AppCompatActivity {
     }
 */
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_main);
         action_bar = (android.support.v7.widget.Toolbar) findViewById(R.id.setting_bar);
         setSupportActionBar(action_bar);
 
-        okbutton = (Button)findViewById(R.id.button);
-        totalEdit   = (EditText)findViewById(R.id.editText);
-        timeEdit   = (EditText)findViewById(R.id.resetText);
+        totalEdit = (EditText) findViewById(R.id.editText);
+        timeEdit = (EditText) findViewById(R.id.resetText);
 
+        //set up sharedPreferences
+        settings = getSharedPreferences(PREFS_NAME, 0);
+        goal = settings.getFloat("Goal", 0);
+        base_measurement = settings.getString("Measure", "oz");
+        //set up editText with values
+        totalEdit.setText(String.valueOf(goal));
+
+
+        rg = (RadioGroup) findViewById(R.id.radioButtonGroup);
+        ounces = (RadioButton) findViewById(R.id.ounces);
+        milli = (RadioButton) findViewById(R.id.ml);
+
+        if (base_measurement.equals("oz")) {
+            ounces.setChecked(true);
+        } else {
+            milli.setChecked(true);
+        }
+
+        toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
         mainActivity = new MainActivity();
-
-        okbutton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
-                        total = totalEdit.getText().toString();
-                        totalInt = Integer.parseInt(total);
-                        mainActivity.setTotal(totalInt);
-                        Log.v("EditText", totalEdit.getText().toString());
-                    }
-                }
-        );
 
     }
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+        int checked = rg.getCheckedRadioButtonId();
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radioButton:
-                if (checked)
-                    //ounces
-                    break;
-            case R.id.radioButton2:
-                if (checked)
-                    //ml
-                    break;
+        switch (checked) {
+            case R.id.ounces:
+                base_measurement = "oz";
+                editor = settings.edit();
+                editor.putString("Measure", base_measurement);
+                editor.commit();
+                toast.makeText(Setting.this, "ounces checked", toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ml:
+                base_measurement = "ml";
+                editor = settings.edit();
+                editor.putString("Measure", base_measurement);
+                editor.commit();
+                toast.makeText(Setting.this, "ml checked", toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
         }
     }
 
@@ -95,6 +119,10 @@ public class Setting extends AppCompatActivity {
 //        }
         int id = item.getItemId();
         if (id == R.id.close) {
+            goal = Float.parseFloat(totalEdit.getText().toString());
+            editor = settings.edit();
+            editor.putFloat("Goal", goal);
+            editor.commit();
             this.finish();
             return true;
         }
@@ -102,7 +130,4 @@ public class Setting extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public int getTotal() {
-        return totalInt;
-    }
 }
